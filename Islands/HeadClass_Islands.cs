da2204904a3f9522f079f14c7b3ba634177675de
jan0mann <jan.ferchof@icloud.com>
2024-10-30 19:35:23 +0100

    fixing a minor thing

--- Islands/HeadClass_Islands.cs
+++ Islands/HeadClass_Islands.cs
@@ -1,7 +1,20 @@
+
 namespace OperationHav
 {
     public class Island
     {
-        
+        public Island(string v1, string v2)
+        {
+        }
+
+        internal void SetExit(string v, Island outside)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void SetExits(object value, Island theatre, Island lab, Island pub)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
