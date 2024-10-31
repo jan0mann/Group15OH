8f167fe1d2dc91eb95e2aa45ec7e22bf5f16b37d
noah2564 <noah.bokelmannq@web.de>
2024-10-31 11:58:32 +0100

    Thursday morning

--- IslandClasses.cs
+++ IslandClasses.cs
@@ -2,9 +2,9 @@ namespace OperationHav
 {
     public class Island
     {
-        public string ShortDescription { get; private set; }
-        public string LongDescription { get; private set;}
-        public Dictionary<string, Island> Exits { get; private set; } = new();
+        public string ShortDescription { get; set; }
+        public string LongDescription { get; set;}
+        public Dictionary<string, Island> Exits { get; set; } = new();
 
         public Island(string shortDesc, string longDesc)
         {
@@ -24,6 +24,83 @@ namespace OperationHav
         {
             if (neighbor != null)
                 Exits[direction] = neighbor;
+        }
+    }
+
+
+
+    // Here ends the headclass for all the islands. 
+    // Below you'll find the subclasses for each island.
+
+
+
+    //Bartek and Noah, please use this class for your island/minigame
+    public class IslandIndustrial : Island 
+    {   
+        public IslandIndustrial(string shortDesc, string longDesc) : base(shortDesc, longDesc)
+        {
+            ShortDescription = shortDesc;
+            LongDescription = longDesc;
+        }
+
+        //You might wanne use this method here for the game itself
+        public void IndustrialWaste()
+        {
+
+        }
+    }
+
+
+
+        //Marcel and Jan, please use this class for your island/minigame
+       public class IslandOil : Island 
+    {   
+        public IslandOil(string shortDesc, string longDesc) : base(shortDesc, longDesc)
+        {
+            ShortDescription = shortDesc;
+            LongDescription = longDesc;
+        }
+
+        //You might wanne use this method here for the game itself
+        public void OilSpill()
+        {
+
+        }
+    }
+
+
+
+        //serafeim and Darius, please use this class for your island/minigame
+       public class IslandPlastic : Island 
+    {   
+        public IslandPlastic(string shortDesc, string longDesc) : base(shortDesc, longDesc)
+        {
+            ShortDescription = shortDesc;
+            LongDescription = longDesc;
+        }
+
+        //You might wanne use this method here for the game itself
+        public void PlasticWaste()
+        {
+
+        }
+    }
+
+
+
+     //On this island/minigame, we all work together (Darius can create the maze for this game now, of course)
+       public class IslandCoral : Island 
+    {   
+        public IslandCoral(string shortDesc, string longDesc) : base(shortDesc, longDesc)
+        {
+            ShortDescription = shortDesc;
+            LongDescription = longDesc;
+        }
+
+        //We might wanne use this method here for the game itself
+        public void CoralReef()
+        {
+
         }
     }
 }
