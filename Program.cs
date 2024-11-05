aeb61ad6f068d255704db43377e809ddbd4589e1
MarcelFedorV <fedorovicimarcel@gmail.com>
2024-11-05 14:57:01 +0200

    Fixing Harbor Command

--- Program.cs
+++ Program.cs
@@ -81,7 +81,7 @@ namespace OperationHav
     public class Program
     {
         public static void Main()
-        {
+        {   
             Game game = new();
             game.Play();
         }
